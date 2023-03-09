974bf8453598d021f3bba66cd6bd5cd4fb9e20b1
EOGames <113043641+EOGames@users.noreply.github.com>
2023-03-09 15:53:40 +0530

    Now code can apend with old file without taking manual backup

--- FileReader.py
+++ FileReader.py
@@ -12,3 +12,14 @@ with open ('ReadFile.txt','w') as file:
 with open ('ReadFile.txt','r') as file:
     newInfo = file.read()
     print(newInfo + " New Edited File")
+
+with open ("newfile.txt",'a') as file: #it will create new file if its not their and will apend stuff u need means will not delete stuff #like w (write) do
+    file.write("Its Added By Code")
+    file.write("\n Its Secound line")
+    file.write ("\n \t New Line And Tabbed")
+
+
+with open ('newfile.txt','r') as file:
+    print(file.read())
+
+
